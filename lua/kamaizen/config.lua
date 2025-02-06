7f5ce8cf53400831dc4d839251477efd35eed878
batoaqaa <ahmed.batoaq@gmail.com>
2025-02-06 16:01:52 +0300

    install LSP and treesitter parser for kamailio configuration files (#1)
    merge before cleanup

--- lua/kamaizen/config.lua
+++ lua/kamaizen/config.lua
@@ -1,18 +1,43 @@
-local M = {}
+local state = { autocmd = {} }
 
--- Define default configuration settings
-M.config = {
-	KamailioSourcePath = "",
-	loglevel = 3,
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.foldingRange = { dynamicRegistration = false, lineFoldingOnly = true }
+
+local server = {
+  name = 'KamaiZen',
+  cmd = { vim.fn.stdpath 'data' .. '/lazy/KamaiZen/KamaiZen' },
+
+  cmd_cwd = vim.fn.getcwd(),
+  filetypes = { 'cfg', 'inc', 'kamailio' },
+  root_dir = vim.fn.getcwd(),
+  capabilities = capabilities,
+  autostart = true,
+  settings = {
+    kamaizen = {
+      enableDeprecatedCommentHint = false, -- to enable hints for '#' comments
+      KamailioSourcePath = vim.fn.getcwd(),
+      loglevel = 3,
+    },
+  },
+  on_init = function(client, results)
+    if results.offsetEncoding then
+      client.offset_encoding = results.offsetEncoding
+    end
+
+    -- if client.config.settings then
+    --   client.notify('workspace/didChangeConfiguration', {
+    --     settings = client.config.settings,
+    --   })
+    -- end
+  end,
+  --on_exit = function(code, signal, client_id)
+  on_exit = function(_, _, client_id)
+    vim.schedule(function()
+      vim.api.nvim_del_autocmd(state.autocmd[client_id])
+    end)
+  end,
 }
 
--- Function to setup the configuration with user overrides
-function M.setup(user_config)
-	if user_config then
-		for key, value in pairs(user_config) do
-			M.config[key] = value
-		end
-	end
-end
-
-return M
+return {
+  server_opts = server,
+}
