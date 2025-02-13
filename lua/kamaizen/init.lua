7c185f0d2536457644358956141d2ab079c46c87
batoaqaa <ahmed.batoaq@gmail.com>
2025-02-13 05:30:17 +0300

    Update

--- lua/kamaizen/init.lua
+++ lua/kamaizen/init.lua
@@ -1,55 +1,73 @@
-local config = require 'kamaizen.config'
+vim.filetype.add {
+  extension = {
+    -- change *.cfg files to kamailio file type only for any of the below condition
+    cfg = function(path, bufnr)
+      --Special Regex Characters: ., +, *, ?, ^, $, (, ), [, ], {, }, |, \
+      if vim.fn.search [[^\s*#!\(KAMAILIO\|OPENSER\|SER\|ALL\|MAXCOMPAT\)]] > 0 then
+        vim.api.nvim_win_set_cursor(0, { 1, 0 })
+        return 'kamailio'
+      elseif vim.fn.search [[^\s*\(request_r\|r\|branch_r\|failure_r\|reply_r\|onreply_r\|onsend_r\|event_r\)oute.*\_s*{\s*]] > 0 then
+        vim.api.nvim_win_set_cursor(0, { 1, 0 })
+        return 'kamailio'
+      elseif vim.fn.search [[^\s*\(#\|!\)!\(define\|ifdef\|ifndef\|endif\|subst\|substdef\)]] > 0 then
+        vim.api.nvim_win_set_cursor(0, { 1, 0 })
+        return 'kamailio'
+      elseif vim.fn.search [[^\s*modparam\s*(\s*"[^"]\+"]] > 0 then
+        vim.api.nvim_win_set_cursor(0, { 1, 0 })
+        return 'kamailio'
+      elseif vim.fn.search [[^\s*loadmodule\s]] > 0 then
+        vim.api.nvim_win_set_cursor(0, { 1, 0 })
+        return 'kamailio'
+      elseif vim.fn.search [[^\s*include_file]] > 0 then
+        vim.api.nvim_win_set_cursor(0, { 1, 0 })
+        return 'kamailio'
+      end
+    end,
+  },
+  filename = {
+    ['kamctlrc'] = 'kamailio', -- if file name is 'kamctlrc' chane file type to kamailio
+  },
+  pattern = {
+    ['.*'] = {
+      --set files that start with '#!KAMAILIO ,#!OPENSER, #!SER ,#!ALL or #!MAXCOMPAT' as kamailio file type
+      function(_, bufnr)
+        local content = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ''
+        if vim.regex([[^\s*#!\(KAMAILIO\|OPENSER\|SER\|ALL\|MAXCOMPAT\)]]):match_str(content) ~= nil then
+          return 'kamailio'
+        end
+      end,
+    },
+  },
+}
+---------------------------------------------------------------------------------------------------------------
+local parsers = require 'nvim-treesitter.parsers'
+local parser_config = parsers.get_parser_configs()
+if not parser_config['kamailio'] then
+  parser_config['kamailio'] = {
+    install_info = {
+      url = 'https://github.com/IbrahimShahzad/tree-sitter-kamailio-cfg',
+      files = { 'src/parser.c' }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+      -- optional entries:
+      branch = 'v0.1.2', -- parser version
+      generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+      requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+    },
+    filetype = 'kamailio', -- if filetype does not match the parser name
+  }
+end
+if parser_config['kamailio'] and not parsers.has_parser 'kamailio' then
+  vim.cmd 'TSInstallSync kamailio'
+end
+---------------------------------------------------------------------------------------------------------------
 local M = {}
 
 M.setup = function(opts)
-  opts = vim.tbl_deep_extend('force', config.server_opts or {}, opts)
-
-  vim.filetype.add {
-    extension = {
-      cfg = 'kamailio',
-    },
-    filename = {
-      ['kamctlrc'] = 'kamailio',
-    },
-    pattern = {
-      ['.*'] = {
-        --set files that start with '#!KAMAILIO' as kamailio file type
-        function(_, bufnr)
-          local content = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ''
-          if vim.regex([[^#!.*KAMAILIO]]):match_str(content) ~= nil then
-            return 'kamailio'
-          end
-        end,
-      },
-    },
-  }
-
+  local lsp_config = require('kamaizen.config').server_opts
+  lsp_config = vim.tbl_deep_extend('force', lsp_config or {}, opts)
   vim.api.nvim_create_autocmd('FileType', {
     pattern = 'kamailio',
     callback = function()
-      ----------------------------------------
-      local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-      local parsers = require 'nvim-treesitter.parsers'
-
-      if not parsers.has_parser 'kamailio' then
-        parser_config['kamailio'] = {
-          install_info = {
-            url = 'https://github.com/IbrahimShahzad/tree-sitter-kamailio-cfg',
-            files = { 'src/parser.c' }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-            branch = 'v0.1.2', -- parser version
-            -- optional entries:
-            generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-            requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-          },
-          filetype = 'kamailio', -- if filetype does not match the parser name
-        }
-
-        vim.schedule_wrap(function()
-          vim.cmd 'TSInstall kamailio'
-        end)()
-      end
-      ----------------------------------------
-      local id = vim.lsp.start(opts)
+      local id = vim.lsp.start(lsp_config)
       if not id then
         vim.notify('Failed to start LSP client', vim.log.levels.ERROR)
         return
