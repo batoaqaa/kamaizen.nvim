46f84b2979e381a79c6d5cd8682ec07381a2f35a
batoaqaa <ahmed.batoaq@gmail.com>
2025-02-13 20:47:06 +0300

    Update

--- lua/kamaizen/init.lua
+++ lua/kamaizen/init.lua
@@ -18,7 +18,7 @@ vim.filetype.add {
       elseif vim.fn.search [[^\s*loadmodule\s]] > 0 then
         vim.api.nvim_win_set_cursor(0, { 1, 0 })
         return 'kamailio'
-      elseif vim.fn.search [[^\s*include_file]] > 0 then
+      elseif vim.fn.search [[^\s*\(include\|import\)_file]] > 0 then
         vim.api.nvim_win_set_cursor(0, { 1, 0 })
         return 'kamailio'
       end
